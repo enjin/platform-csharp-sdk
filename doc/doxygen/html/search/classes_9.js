bc223f135b5b343fbdfdf66d080aa3c7c1b7a475
Bishop S Johnson <bishop@enjin.io>
2023-08-16 16:26:40 -0700

    Updated Doxygen files

--- doc/doxygen/html/search/classes_9.js
+++ doc/doxygen/html/search/classes_9.js
@@ -4,8 +4,10 @@ var searchData=
   ['listingdatafragment_1',['ListingDataFragment',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_data_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
   ['listingdatafragment_3c_20auctiondatafragment_20_3e_2',['ListingDataFragment&lt; AuctionDataFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_data_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
   ['listingdatafragment_3c_20fixedpricedatafragment_20_3e_3',['ListingDataFragment&lt; FixedPriceDataFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_data_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
-  ['listingstate_4',['ListingState',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state.html',1,'Enjin::Platform::Sdk::Marketplace']]],
-  ['listingstatefragment_5',['ListingStateFragment',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
-  ['listingstatefragment_3c_20auctionstatefragment_20_3e_6',['ListingStateFragment&lt; AuctionStateFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
-  ['listingstatefragment_3c_20fixedpricestatefragment_20_3e_7',['ListingStateFragment&lt; FixedPriceStateFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]]
+  ['listingdatajsonconverter_4',['ListingDataJsonConverter',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_data_json_converter.html',1,'Enjin::Platform::Sdk::Marketplace']]],
+  ['listingstate_5',['ListingState',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state.html',1,'Enjin::Platform::Sdk::Marketplace']]],
+  ['listingstatefragment_6',['ListingStateFragment',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
+  ['listingstatefragment_3c_20auctionstatefragment_20_3e_7',['ListingStateFragment&lt; AuctionStateFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
+  ['listingstatefragment_3c_20fixedpricestatefragment_20_3e_8',['ListingStateFragment&lt; FixedPriceStateFragment &gt;',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_fragment.html',1,'Enjin::Platform::Sdk::Marketplace']]],
+  ['listingstatejsonconverter_9',['ListingStateJsonConverter',['../class_enjin_1_1_platform_1_1_sdk_1_1_marketplace_1_1_listing_state_json_converter.html',1,'Enjin::Platform::Sdk::Marketplace']]]
 ];
